994bb77806d30b8778b686675c9435b064c9ff3f
Rakeshkumar <pidikiralarakeshkumar@gmail.com>
2026-05-30 16:05:40 +0530

    Create CreateOrderRequest.java

---
+++ backend/src/main/java/com/devops/chaikafe/entity/CreateOrderRequest.java
@@ -0,0 +1,13 @@
+package com.devops.chaikafe.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class CreateOrderRequest {
+    private Long userId;
+    private List<OrderItemRequest> items;
+}
