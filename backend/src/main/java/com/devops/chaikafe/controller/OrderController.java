0b3ee368e91447050375652a9c61bff69a8c5d9f
Rakeshkumar <pidikiralarakeshkumar@gmail.com>
2026-05-30 16:09:08 +0530

    Update OrderController.java

--- backend/src/main/java/com/devops/chaikafe/controller/OrderController.java
+++ backend/src/main/java/com/devops/chaikafe/controller/OrderController.java
@@ -1,35 +1,28 @@
 package com.devops.chaikafe.controller;
 
-import com.devops.chaikafe.dto.OrderRequestDto;
-import com.devops.chaikafe.dto.OrderResponseDto;
+import com.devops.chaikafe.dto.CreateOrderRequest;
 import com.devops.chaikafe.entity.Order;
 import com.devops.chaikafe.service.OrderService;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
-@CrossOrigin(origins = "http://localhost:5173")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class OrderController {
 
-    @Autowired
-    private OrderService orderService;
+    private final OrderService orderService;
 
     @PostMapping
-    public OrderResponseDto createOrder(@RequestBody OrderRequestDto orderRequestDto) {
-        return orderService.createOrder(orderRequestDto);
+    public Order createOrder(@RequestBody CreateOrderRequest request) {
+        return orderService.createOrder(request);
     }
 
     @GetMapping
     public List<Order> getAllOrders() {
         return orderService.getAllOrders();
-    }
-
-    @GetMapping("/{id}")
-    public Order getOrderById(@PathVariable Long id) {
-        return orderService.getOrderById(id);
     }
 }
